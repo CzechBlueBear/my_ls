8610c32cd149878fd1c44a223318d8e134095a46
Jiri Bluebear Dluhos <jiri.bluebear.dluhos@gmail.com>
2025-01-12 06:20:02 +0100

    Make code more Rust-style (hopefully).

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,173 @@
 use std::fs;
 use std::env;
+use std::os::unix::fs::MetadataExt;
 use std::process;
 use std::os::unix::fs::FileTypeExt;
 
-/// File types, as reported in our listing.
-#[derive(PartialEq)]
-enum ListingFileType {
-    Unknown,
-    Regular,
-    Directory,
-    Symlink,
-    Pipe,
-    Socket,
-    CharDevice,
-    BlockDevice
-}
+const ICON_ERROR:   &'static str = "\u{2753}\u{FE0E}";
+const ICON_FILE:    &'static str = "\u{1F5CE}\u{FE0E} ";
+const ICON_DIRECTORY:  &'static str = "\u{1F4C1}\u{FE0E}";
+const ICON_SYMLINK: &'static str = "\u{1F517}\u{FE0E}";
+const ICON_EMPTY_FILE: &'static str = "\u{2B55}\u{FE0E}";
+const ICON_SOCKET:  &'static str = "\u{1F50C}\u{FE0E}";
+const ICON_PIPE:    &'static str = "\u{1F6B0}\u{FE0E}";
+const ICON_TEXT_FILE: &'static str = "\u{1F5D2}\u{FE0E}";
+const ICON_CHAR_DEVICE: &'static str = "\u{1F5A8}\u{FE0E}";
+const ICON_BLOCK_DEVICE: &'static str = "\u{1F4BF}\u{FE0E}";
+const ICON_DISK:    &'static str = "\u{1F5D4}\u{FE0E}";
+const ICON_DEV_NULL:  &'static str = "\u{1F6BD}\u{FE0E}";
+const ICON_TTY:     &'static str = "\u{1F4BB}\u{FE0E}";
 
 /// A single entry of the listing we will produce.
-struct ListingEntry {
-    pub name: String,
-    pub file_type: ListingFileType,
-    pub link_target: String
+#[derive(PartialEq, Eq)]
+enum ListingEntry {
+
+    Unknown {
+        name: String,
+        icon: String
+    },
+    Regular {
+        name: String,
+        icon: String
+    },
+    Directory {
+        name: String,
+        icon: String
+    },
+    Symlink {
+        name: String,
+        target: String,
+        icon: String
+    },
+    Pipe {
+        name: String,
+        icon: String
+    },
+    Socket {
+        name: String,
+        icon: String
+    },
+    CharDevice {
+        name: String,
+        dev_id: u64,
+        icon: String
+    },
+    BlockDevice {
+        name: String,
+        dev_id: u64,
+        icon: String
+    }
 }
 
 impl ListingEntry {
 
-    pub fn new(name: &str, file_type: ListingFileType) -> ListingEntry {
-        ListingEntry {
-            name: name.to_string(),
-            file_type,
-            link_target: "".to_string()
+    pub fn get_name(&self) -> String {
+        match self {
+            ListingEntry::Unknown { name, .. } => { name.to_string() }
+            ListingEntry::Regular { name, .. } => { name.to_string() }
+            ListingEntry::Directory { name, .. } => { name.to_string() }
+            ListingEntry::Symlink { name, .. } => { name.to_string() }
+            ListingEntry::Pipe { name, .. } => { name.to_string() }
+            ListingEntry::Socket { name, .. } => { name.to_string() }
+            ListingEntry::CharDevice { name, .. } => { name.to_string() }
+            ListingEntry::BlockDevice { name, .. } => { name.to_string() }
         }
     }
 
-    pub fn new_symlink(name: &str, link_target: &str) -> ListingEntry {
-        ListingEntry {
+    pub fn get_icon(&self) -> String {
+        match self {
+            ListingEntry::Unknown { icon, .. } => { icon.to_string() }
+            ListingEntry::Regular { icon, .. } => { icon.to_string() }
+            ListingEntry::Directory { icon, .. } => { icon.to_string() }
+            ListingEntry::Symlink { icon, .. } => { icon.to_string() }
+            ListingEntry::Pipe { icon, .. } => { icon.to_string() }
+            ListingEntry::Socket { icon, .. } => { icon.to_string() }
+            ListingEntry::CharDevice { icon, .. } => { icon.to_string() }
+            ListingEntry::BlockDevice { icon, .. } => { icon.to_string() }
+        }
+    }
+
+    pub fn is_directory(&self) -> bool {
+        match self {
+            ListingEntry::Directory { .. } => { true }
+            _ => { false }
+        }
+    }
+
+    pub fn new_regular(name: &str) -> ListingEntry {
+        ListingEntry::Regular {
             name: name.to_string(),
-            file_type: ListingFileType::Symlink,
-            link_target: link_target.to_string()
+            icon: ICON_FILE.into()
+        }
+    }
+
+    pub fn new_dir(name: &str) -> ListingEntry {
+        ListingEntry::Directory {
+            name: name.to_string(),
+            icon: ICON_DIRECTORY.into()
+        }
+    }
+
+    pub fn new_symlink(name: &str, target: &str) -> ListingEntry {
+        ListingEntry::Symlink {
+            name: name.to_string(),
+            target: target.to_string(),
+            icon: ICON_SYMLINK.into()
+        }
+    }
+
+    pub fn new_unknown(name: &str) -> ListingEntry {
+        ListingEntry::Unknown {
+            name: name.to_string(),
+            icon: ICON_ERROR.into()
+        }
+    }
+
+    pub fn new_pipe(name: &str) -> ListingEntry {
+        ListingEntry::Pipe {
+            name: name.to_string(),
+            icon: ICON_PIPE.into()
+        }
+    }
+
+    pub fn new_char_device(name: &str, dev_id: u64) -> ListingEntry {
+        let mut icon = ICON_CHAR_DEVICE;
+
+        // give some specific devices their own icons
+        let dev_major = (dev_id & 0x000000000000ff00) >> 8;
+        let dev_minor = dev_id & 0x00000000000000ff;
+        if dev_major == 1 && dev_minor == 3 {   // /dev/null
+            icon = ICON_DEV_NULL;
+        }
+        else if dev_major == 4 {                // oldschool ttys
+            icon = ICON_TTY;
+        }
+        else if dev_major == 5 && (dev_minor == 0 || dev_minor == 1) {      // /dev/tty, /dev/console
+            icon = ICON_TTY;
+        }
+        else if dev_major == 241 {              // disks
+            icon = ICON_DISK;
+        }
+
+        ListingEntry::CharDevice {
+            name: name.to_string(),
+            dev_id: dev_id,
+            icon: icon.into()
+        }
+    }
+
+    pub fn new_block_device(name: &str, dev_id: u64) -> ListingEntry {
+        ListingEntry::BlockDevice {
+            name: name.to_string(),
+            dev_id: dev_id,
+            icon: ICON_BLOCK_DEVICE.into()
+        }
+    }
+
+    pub fn new_socket(name: &str) -> ListingEntry {
+        ListingEntry::Socket {
+            name: name.to_string(),
+            icon: ICON_SOCKET.into()
         }
     }
 
@@ -47,7 +177,7 @@ impl ListingEntry {
         // we print "???" to at least show that there is something
         let name = dentry.file_name().into_string();
         if name.is_err() {
-            return ListingEntry::new("???", ListingFileType::Unknown);
+            return ListingEntry::new_unknown("???");
         }
         let name = name.unwrap();
 
@@ -55,12 +185,12 @@ impl ListingEntry {
         // we print the name and unknown type
         let dentry_file_type = dentry.file_type();
         if dentry_file_type.is_err() {
-            return ListingEntry::new(&name, ListingFileType::Unknown);
+            return ListingEntry::new_unknown(&name);
         }
         let dentry_file_type = dentry_file_type.unwrap();
 
         if dentry_file_type.is_dir() {
-            ListingEntry::new(&name, ListingFileType::Directory)
+            ListingEntry::new_dir(&name)
         }
         else if dentry_file_type.is_symlink() {
             let result = fs::read_link(dentry.path());
@@ -79,56 +209,46 @@ impl ListingEntry {
             }
         }
         else if dentry_file_type.is_fifo() {
-            ListingEntry::new(&name, ListingFileType::Pipe)
+            ListingEntry::new_pipe(&name)
         }
         else if dentry_file_type.is_char_device() {
-            ListingEntry::new(&name, ListingFileType::CharDevice)
+            let result = dentry.metadata();
+            match result {
+                Err(_) => { ListingEntry::new_char_device(&name, 0) }
+                Ok(metadata) => {
+                    let dev_id = metadata.rdev();
+                    ListingEntry::new_char_device(&name, dev_id)
+                }
+            }
         }
         else if dentry_file_type.is_block_device() {
-            ListingEntry::new(&name, ListingFileType::BlockDevice)
+            let result = dentry.metadata();
+            match result {
+                Err(_) => { ListingEntry::new_block_device(&name, 0) }
+                Ok(metadata) => {
+                    let dev_id = metadata.rdev();
+                    ListingEntry::new_block_device(&name, dev_id)
+                }
+            }
         }
         else if dentry_file_type.is_socket() {
-            ListingEntry::new(&name, ListingFileType::Socket)
+            ListingEntry::new_socket(&name)
         }
         else {
-            ListingEntry::new(&name, ListingFileType::Regular)
+            ListingEntry::new_regular(&name)
         }
-    }
-
-    /// Returns a string containing Unicode icon for the file.
-    pub fn get_icon(&self) -> &'static str {
-        match self.file_type {
-            ListingFileType::Unknown => { "\u{274E}\u{FE0E}" },
-            ListingFileType::Regular => { "\u{1F5CE}\u{FE0E} " },
-            ListingFileType::Directory => { "\u{1F4C1}\u{FE0E}" },
-            ListingFileType::Symlink => { "\u{1F517}\u{FE0E}" },
-            ListingFileType::Pipe => { "\u{1F6B0}\u{FE0E}" },
-            ListingFileType::Socket => { "\u{1F50C}\u{FE0E}" },
-            ListingFileType::CharDevice => { "\u{1F5A8}\u{FE0E}" },
-            ListingFileType::BlockDevice => { "\u{1F4BF}\u{FE0E}" }
-        }
-    }
-}
-
-impl PartialEq for ListingEntry {
-    fn eq(&self, other: &Self) -> bool {
-        self.name == other.name &&
-            self.file_type == other.file_type
     }
 }
 
 impl PartialOrd for ListingEntry {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.name.partial_cmp(&other.name)
+        self.get_name().partial_cmp(&other.get_name())
     }
-}
-
-impl Eq for ListingEntry {
 }
 
 impl Ord for ListingEntry {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.name.cmp(&other.name)
+        self.get_name().cmp(&other.get_name())
     }
 }
 
@@ -154,7 +274,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             // if the query fails, add at least the "???" entry
             // to show that something was detected
-            listing.push(ListingEntry::new("???", ListingFileType::Unknown));
+            listing.push(ListingEntry::new_unknown("???"));
         }
     }
 
@@ -162,88 +282,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // show directories first
     for l in &listing {
-        if l.file_type == ListingFileType::Directory {
-            println!("{} {}", l.get_icon(), l.name);
+        if l.is_directory() {
+            println!("{} {}", l.get_icon(), l.get_name());
         }
     }
 
     // then other files
     for l in &listing {
-        if l.file_type != ListingFileType::Directory {
-            if l.file_type == ListingFileType::Symlink {
-                println!("{} {} -> {}", l.get_icon(), l.name, l.link_target);
-            } else {
-                println!("{} {}", l.get_icon(), l.name);
+        match l {
+            ListingEntry::Directory {..} => { },
+            ListingEntry::Symlink { name, icon, target } => {
+                println!("{} {} -> {}", icon, name, target);
+            }
+            _ => {
+                println!("{} {}", l.get_icon(), l.get_name());
             }
         }
     }
 
     Ok(())
-}
-
-fn might_be_dir(dentry: &fs::DirEntry) -> bool
-{
-    match dentry.file_type() {
-        Ok(file_type) => { file_type.is_dir() },
-        Err(_) => { false }
-    }
-}
-
-fn print_dir_entry(dentry: &fs::DirEntry)
-{
-    const ICON_ERROR:   &'static str = "\u{2753}\u{FE0E}";
-    const ICON_FILE:    &'static str = "\u{1F5CE}\u{FE0E} ";
-    const ICON_FOLDER:  &'static str = "\u{1F4C1}\u{FE0E}";
-    const ICON_CHAIN:   &'static str = "\u{1F517}\u{FE0E}";
-    const ICON_EMPTY_FILE: &'static str = "\u{2B55}\u{FE0E}";
-    const ICON_SOCKET:  &'static str = "\u{1F50C}\u{FE0E}";
-    const ICON_PIPE:    &'static str = "\u{1F6B0}\u{FE0E}";
-    const ICON_TEXT_FILE: &'static str = "\u{1F5D2}\u{FE0E}";
-
-    let name = dentry.file_name().into_string();
-    if name.is_err() {
-        println!("{} (error reading name)", ICON_ERROR);
-        return;
-    }
-    let name = name.unwrap();
-
-    let mut icon:&str = ICON_FILE;
-
-    let file_type = dentry.file_type();
-    if file_type.is_err() {
-        println!("{} {}", ICON_ERROR, name);
-        return;
-    }
-    let file_type = file_type.unwrap();
-
-    if file_type.is_dir() {
-        icon = ICON_FOLDER;
-    }
-    if file_type.is_symlink() {
-        icon = ICON_CHAIN;
-    };
-    if file_type.is_socket() {
-        icon = ICON_SOCKET;
-    }
-    if file_type.is_fifo() {
-        icon = ICON_PIPE;
-    }
-
-    if file_type.is_file() {
-        if name.ends_with(".txt") {
-            icon = ICON_TEXT_FILE;
-        }
-        else {
-            match dentry.metadata() {
-                Ok(metadata) => {
-                    if metadata.len() == 0 {
-                        icon = ICON_EMPTY_FILE;
-                    }
-                },
-                Err(_) => {}
-            }
-        }
-    }
-
-    println!("{} {}", icon, name);
 }
